d00ae6180a69e1865a6168e07f965ac2d11f7019
Cody Caro <codyccaro@gmail.com>
2017-03-02 00:21:45 -0800

    Started Sprite Animation

--- Main.cpp
+++ Main.cpp
@@ -5,34 +5,45 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "First SFML Game");
+	enum Direction { Left, Right };
 
-	std::string message = "Hello my name is Cody";
-	std::string display = "";
+	sf::Vector2i source(32, Right);
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "First SFML Game");
 
 	int index = 0;
 
 	window.setKeyRepeatEnabled(false);
 
+	sf::Texture pTexture;
+	sf::Sprite playerImage;
+	if (!pTexture.loadFromFile("Player.png")) {
+		std::cout << "Error could not load player image" << std::endl;
+	}
+
+	playerImage.setTexture(pTexture);
+
+	playerImage.setPosition(100, 100);
+
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) 
-				window.close();
 
-			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.code == sf::Keyboard::Return) {
-					display += message[index];
-					index++;
-					system("cls");
-					std::cout << display;
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (event.key.code == sf::Keyboard::Right) {
+					source.y = Right;
+				}
+				else if (event.key.code == sf::Keyboard::Left) {
+					source.y = Left;
 				}
 			}
 		}
 		
-		
-		
-
+		window.draw(playerImage);
 		window.display();
 	}
 	
